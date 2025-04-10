915c723a0dea454e8195cea1bc4567c6530d523f
dunamismax <dunamismax@tutamail.com>
2025-04-10 15:03:30 -0400

    u

---
+++ xcode/RemindersApp/RemindersAppTests/RemindersAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  RemindersAppTests.swift
+//  RemindersAppTests
+//
+//  Created by Stephen Sawyer on 4/10/25.
+//
+
+import Testing
+
+struct RemindersAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
